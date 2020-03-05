3a9b0488c4ed6a86cd718982aad57c44ff6a3927
ravi tejpal <tejpal.ravi1985@gmail.com>
2020-03-05 21:59:28 +0530

    Create Procedure

---
+++ Adv-SSDT/Adv-SSDT/dbo/Stored Procedures/GetEmployeeSales.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE Employee_sales (@empid int ,@SalesAmount Money out)
+AS
+	BEGIN TRY
+	IF NOT EXISTS (Select * from HumanResources.Employee  where BusinessEntityID =@empid )
+	GOTO tt;
+	ELSE
+	Select  @SalesAmount =sum(h.SubTotal)   from Sales.SalesOrderHeader h where SalesPersonID =@empid 
+	END TRY
+
+	BEGIN CATCH
+	
+	tt:
+	RAISERROR('Employee Does not exists',0,0)
+	END CATCH
+RETURN 0
